e9406cf148d736d2e4fb24764141a1ca15276abc
Gabriele <carretta63@gmail.com>
2010-08-11 18:44:07 +0200

    Added getToken

--- Grooveshark.py
+++ Grooveshark.py
@@ -1,15 +1,23 @@
 import urllib2
+import httplib2
 import hashlib
 import uuid
+import json
 
 listen_url = 'http://listen.grooveshark.com/'
-cowbell_url = 'http://cowbell.grooveshark.com/service.php'
+cowbell_url = 'https://cowbell.grooveshark.com/service.php'
+six_hex = hashlib.md5('Groovepy').hexdigest()[0:6]
+groove_salt = 'quitStealinMahShit'
+
+header = {}
+header['user-agent'] = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)'
 
 class Grooveshark:
 	def __init__(self):
 		self.sessionID = None
 		self.secretKEY = None
-		self.uuid = uuid.uuid4()
+		self.token = None
+		self.uuid = str(uuid.uuid4())
 		
 	def getSessionID(self):
 		req = urllib2.urlopen(listen_url)
@@ -21,11 +29,33 @@ class Grooveshark:
 	def getSecretKEY(self):
 		m = hashlib.md5(self.sessionID)
 		self.secretKEY = m.hexdigest()
+
+	def getToken(self):
+		http = httplib2.Http()
+		postdata = {"header":
+						{"session":self.sessionID,
+						 "uuid":self.uuid,
+						 "client":"gslite",
+						 "clientRevision":"20100412.81"},
+					"parameters":
+						{"secretKey":self.secretKEY},
+					"method":"getCommunicationToken"}
+		json_postdata = json.dumps(postdata)
+		req = urllib2.urlopen(cowbell_url, json_postdata)
+		request, reply = http.request(cowbell_url, 'POST', headers=header, body=json_postdata)
+		self.token = json.loads(reply)['result']
+		
+	def useToken(self, method):
+		tosha = '%s:%s:%s:%s' % (method)
+		m = hashlib.sha1(method)
+		
 		
 
 g = Grooveshark()
 g.getSessionID()
 g.getSecretKEY()
+g.getToken()
 
 print g.sessionID
 print g.secretKEY
+print g.token
